b2d24fa8cad4ba4802d0d0741be7e11ef2f7bb96
Will Handley <wh260@cam.ac.uk>
2019-03-14 16:52:01 +0000

    Added setup.py etc

---
+++ README.rst
@@ -0,0 +1,18 @@
+===========================================
+anesthetic: nested sampling post-processing 
+===========================================
+:anesthetic: nested sampling post-processing
+:Author: Will Handley
+:Version: 0.0.0
+:Homepage: https://github.com/williamjameshandley/anesthetic
+
+This project is still in alpha stage. It aims to bring together tools for processing nested sampling chains, leveraging the standard python libraries:
+
+- numpy
+- scipy
+- pandas
+
+To Do
+-----
+- tests
+- CI
---
+++ setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+from setuptools import setup, find_packages
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
+def get_version(short=False):
+    with open('README.rst') as f:
+        for line in f:
+            if ':Version:' in line:
+                ver = line.split(':')[2].strip()
+                if short:
+                    subver = ver.split('.')
+                    return '%s.%s' % tuple(subver[:2])
+                else:
+                    return ver
+
+
+setup(name='anesthetic',
+      version=get_version(),
+      description='anesthetic: nested sampling post-processing',
+      long_description=readme(),
+      author='Will Handley',
+      author_email='wh260@cam.ac.uk',
+      url='https://github.com/williamjameshandley/anesthetic',
+      packages=find_packages(),
+      install_requires=['numpy', 'scipy'],
+      setup_requires=['pytest-runner'],
+      extras_require={
+          'docs': ['sphinx', 'sphinx_rtd_theme', 'numpydoc'],
+          },
+      tests_require=['pytest'],
+      include_package_data=True,
+      license='MIT',
+      classifiers=[
+                   'Development Status :: 3 - Alpha'
+                   'Intended Audience :: Developers',
+                   'Intended Audience :: Science/Research',
+                   'Natural Language :: English',
+                   'License :: OSI Approved :: MIT License',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3.4',
+                   'Programming Language :: Python :: 3.5',
+                   'Programming Language :: Python :: 3.6',
+                   'Topic :: Scientific/Engineering',
+                   'Topic :: Scientific/Engineering :: Astronomy',
+                   'Topic :: Scientific/Engineering :: Physics',
+                   'Topic :: Scientific/Engineering :: Visualization',
+                   'Topic :: Scientific/Engineering :: Information Analysis',
+                   'Topic :: Scientific/Engineering :: Mathematics',
+      ],
+      )
