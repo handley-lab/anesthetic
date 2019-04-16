7f8fea472bea0ee423c49b857842aa358779bdd1
Will Handley <wh260@cam.ac.uk>
2019-04-16 12:30:14 +0100

    tests syntastically correct

--- tests/test_gui.py
+++ tests/test_gui.py
@@ -1,21 +1,21 @@
 from anesthetic.gui import RunPlotter
 
+
 def test_gui():
     plotter = RunPlotter('./tests/example_data/ns/ns')
 
     # Type buttons
     plotter.type.buttons.set_active(0)
-    assert(plotter.type()=='live')
+    assert(plotter.type() == 'live')
     plotter.type.buttons.set_active(1)
-    assert(plotter.type()=='posterior')
+    assert(plotter.type() == 'posterior')
 
     # Parameter choice buttons
     plotter.param_choice.buttons.set_active(1)
-    assert(len(plotter.triangle.ax)==2)
+    assert(len(plotter.triangle.ax) == 2)
     plotter.param_choice.buttons.set_active(0)
-    assert(len(plotter.triangle.ax)==1)
+    assert(len(plotter.triangle.ax) == 1)
     plotter.param_choice.buttons.set_active(0)
     plotter.param_choice.buttons.set_active(2)
     plotter.param_choice.buttons.set_active(3)
-    assert(len(plotter.triangle.ax)==4)
-
+    assert(len(plotter.triangle.ax) == 4)
