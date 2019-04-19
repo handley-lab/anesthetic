5427d21ddb489aabdfa3182733b6005720bae6c1
Will Handley <wh260@cam.ac.uk>
2019-04-19 11:05:51 +0100

    Updated demo

--- demo.py
+++ demo.py
@@ -16,8 +16,8 @@ import tarfile
 for filename in ["plikHM_TTTEEE_lowl_lowE_lensing.tar.gz","plikHM_TTTEEE_lowl_lowE_lensing_NS.tar.gz"]:
     github_url = "https://github.com/williamjameshandley/cosmo_example/raw/master/"
     url = github_url + filename
-    open('data/' + filename, 'wb').write(requests.get(url).content)
-    tarfile.open('data/' + filename).extractall()
+    open(filename, 'wb').write(requests.get(url).content)
+    tarfile.open(filename).extractall()
 
 
 #| ## Marginalised posterior plotting
