8eff13578e820671ff3ae41350c8839e67b15c88
Will Handley <wh260@cam.ac.uk>
2019-04-19 08:10:04 +0100

    Reversed order of loading for nested samples

--- anesthetic/read/nested.py
+++ anesthetic/read/nested.py
@@ -6,14 +6,13 @@ from anesthetic.read.multinest import MultiNestReader
 
 def NestedReader(root):
     """Read from a variety of nested sampling chains."""
-    pc = PolyChordReader(root)
-
-    if os.path.isfile(pc.birth_file):
-        return pc
-
     mn = MultiNestReader(root)
     if os.path.isfile(mn.ev_file) and os.path.isfile(mn.phys_live_file):
         return mn
+
+    pc = PolyChordReader(root)
+    if os.path.isfile(pc.birth_file):
+        return pc
 
     errstr = "Could not find nested sampling chains with root %s" % root
     try:
