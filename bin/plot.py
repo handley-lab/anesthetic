7e1737bae3502bbde6e16a2ed4589a69c9619085
Will Handley <wh260@cam.ac.uk>
2019-07-25 21:23:09 +0100

    All contours now plotted

--- bin/plot.py
+++ bin/plot.py
@@ -9,7 +9,7 @@ sigma2 = 0.1                  # x2 parameter
 a, b, m = 2., 4., 0.5         # x4 parameters
 
 n = 1000
-ls = 'r--'
+ls = 'k--'
 
 x = numpy.linspace(-0.4,0.4,n)
 p = numpy.exp(-x**2/sigma0**2/2)/numpy.sqrt(2*numpy.pi)/sigma0
@@ -75,6 +75,10 @@ for p in [0.66, 0.95]:
     x4 = x4 * numpy.ones_like(x3)
     axes['x3']['x4'].plot(x3, x4, ls)
 
+axes['x3']['x4'].plot(x3, numpy.ones_like(x3)*b, ls)
+
+axes['x3']['x4'].plot(x3[0] * numpy.ones(n), numpy.linspace(x4[0],b,n), ls)
+axes['x3']['x4'].plot(x3[-1] * numpy.ones(n), numpy.linspace(x4[0],b,n), ls)
 
 x4 = numpy.linspace(a,b,n)
 for p in [0.66, 0.95]:
@@ -86,4 +90,18 @@ for p in [0.66, 0.95]:
 axes['x2']['x4'].plot(numpy.zeros_like(x4), x4, ls)
 axes['x2']['x4'].plot(x2, b*numpy.ones_like(x2), ls)
 
+from scipy.special import erf, erfi
+from scipy.optimize import root
 
+for p in [0.66, 0.95]:
+    k = root(lambda k: (1/(b-a)**2*(2+(b-a)**2*m)**2*erf(numpy.sqrt(numpy.log((2+(b-a)**2*m)/2/(b-a)/k))) - 4*k**2*erfi(numpy.sqrt(numpy.log((2+(b-a)**2*m)/2/(b-a)/k))))/8/m-p, 0.5).x[0]
+    x1 = numpy.sqrt(2*numpy.log((2+(a-b)**2*m)/2/(b-a)/k))
+    x1 = numpy.linspace(-x1,x1,n)
+    x4 = (a+b)/2 + numpy.exp(x1**2/2)*k/m - 1/(b-a)/m
+    x1 *= sigma1
+    axes['x1']['x4'].plot(x1, x4, ls)
+
+    axes['x0']['x4'].plot(x1, x4, ls)
+
+axes['x1']['x4'].plot(x1, b*numpy.ones_like(x1), ls)
+axes['x0']['x4'].plot(x1, b*numpy.ones_like(x1), ls)
