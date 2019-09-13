d5ba374a0612f1e4b665621ee7e1a7666ad879a6
Will Handley <wh260@cam.ac.uk>
2019-09-13 18:18:57 +0100

    PolyChord can now read live points if necessary

--- anesthetic/read/polychordreader.py
+++ anesthetic/read/polychordreader.py
@@ -3,12 +3,24 @@ import numpy
 from anesthetic.read.getdistreader import GetDistReader
 
 
+try:
+    FileNotFoundError
+except NameError:
+    FileNotFoundError = IOError
+
+
 class PolyChordReader(GetDistReader):
     """Read polychord files."""
 
     def samples(self):
         """Read ``<root>_dead-birth.txt`` in polychord format."""
         data = numpy.loadtxt(self.birth_file)
+        try:
+            _data = numpy.loadtxt(self.phys_live_birth_file)
+            data = numpy.concatenate([data, _data])
+            data = numpy.unique(data, axis=0)
+        except FileNotFoundError:
+            pass
         samples, logL, logL_birth = numpy.split(data, [-2, -1], axis=1)
         return samples, logL.flatten(), logL_birth.flatten()
 
@@ -16,3 +28,8 @@ class PolyChordReader(GetDistReader):
     def birth_file(self):
         """File containing dead and birth contours."""
         return self.root + '_dead-birth.txt'
+
+    @property
+    def phys_live_birth_file(self):
+        """File containing physical live points."""
+        return self.root + '_phys_live-birth.txt'
