set values <-1e150 to -1e150 to fix overflow issues which happen beca… (#71)
* set values <-1e150 to -1e150 to fix overflow issues which happen because of cobaya's logzero=1e300 definition

* add logzero kwarg, set logL<=logzero to -numpy.inf, add logsumexpinf() function to handle -inf input to logsumexp

* test if logL_birth isinstance(int), special MultiNest case for old MN data

* add test for utils function logsumexpinf

* make logsumexpinf docstring conform with convention=numpy

* catch expected warning in test_logsumexpinf

* rename utils.logsumexpinf to utils.logsumexp and replace scipy's logsumexp completely with the utils.py version

* add doc entry for logzero in NestedSamples

* remove unnecessary abbreviation  for scipy's

* remove unnecessary newline

* make utils.logsumexp match signature of scipy.special.logsumexp

* add simple arg test to test_build_mcmc for using logzero kwarg in NestedSamples

* add test for effect of logzero conversion, move logzero conversion from root reading branch (if root is not None) to kwarg branch (else) where it acts on both

* add test for logzero not changing anything and for logzero not affecting rest of logL

* change logzero default from -numpy.inf to -1e30 (as in CosmoMC and MontePython)

* move logzero check up in hierarchy from NestedSamples to MCMCSamples

* add tests for using logzero kwarg with MCMCSamples

* correct indent for PEP8

Co-authored-by: lukas hergt <lh561@cam.ac.uk>

anesthetic/samples.py
@@ -6,13 +6,12 @@
 import os
 import numpy
 import pandas
-from scipy.special import logsumexp
 from anesthetic.plot import (make_1d_axes, make_2d_axes, fastkde_plot_1d,
                              kde_plot_1d, hist_plot_1d, scatter_plot_2d,
                              fastkde_contour_plot_2d,
                              kde_contour_plot_2d, hist_plot_2d)
 from anesthetic.read.samplereader import SampleReader
-from anesthetic.utils import compute_nlive, is_int
+from anesthetic.utils import compute_nlive, is_int, logsumexp
 from anesthetic.gui.plot import RunPlotter
 from anesthetic.weighted_pandas import WeightedDataFrame, WeightedSeries
 
@@ -54,6 +53,11 @@ class MCMCSamples(WeightedDataFrame):
     label: str
         Legend label
 
+    logzero: float
+        The threshold for `log(0)` values assigned to rejected sample points.
+        Anything equal or below this value is set to `-numpy.inf`.
+        default: -1e30
+
     """
 
     def __init__(self, *args, **kwargs):
@@ -75,7 +79,10 @@ class MCMCSamples(WeightedDataFrame):
                           tex=tex, limits=limits, *args, **kwargs)
             self.root = root
         else:
+            logzero = kwargs.pop('logzero', -1e30)
             logL = kwargs.pop('logL', None)
+            if logL is not None:
+                logL = numpy.where(logL <= logzero, -numpy.inf, logL)
             self.tex = kwargs.pop('tex', {})
             self.limits = kwargs.pop('limits', {})
             self.label = kwargs.pop('label', None)
@@ -389,6 +396,11 @@ class NestedSamples(MCMCSamples):
     beta: float
         thermodynamic temperature
 
+    logzero: float
+        The threshold for `log(0)` values assigned to rejected sample points.
+        Anything equal or below this value is set to `-numpy.inf`.
+        default: -1e30
+
     """
 
     def __init__(self, *args, **kwargs):
@@ -405,9 +417,15 @@ class NestedSamples(MCMCSamples):
                           tex=tex, limits=limits, *args, **kwargs)
             self.root = root
         else:
+            logzero = kwargs.pop('logzero', -1e30)
             self._beta = kwargs.pop('beta', 1.)
             logL_birth = kwargs.pop('logL_birth', None)
-            super(NestedSamples, self).__init__(*args, **kwargs)
+            if not isinstance(logL_birth, int) and logL_birth is not None:
+                logL_birth = numpy.where(logL_birth <= logzero,
+                                         -numpy.inf, logL_birth)
+
+            super(NestedSamples, self).__init__(logzero=logzero,
+                                                *args, **kwargs)
             if logL_birth is not None:
                 self._compute_nlive(logL_birth)
 