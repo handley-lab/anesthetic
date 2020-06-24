bc0b9e73c43e16554b73bf4f5d59852191fda9f7
lukashergt <lh561@mrao.cam.ac.uk>
2020-06-24 07:51:07 +0100

    use uniform subsample for 1d compression

--- anesthetic/utils.py
+++ anesthetic/utils.py
@@ -340,14 +340,10 @@ def sample_compression_1d(x, w=None, n=1000):
         w = pandas.Series(index=x.index, data=np.ones_like(x))
 
     # Select inner samples for triangulation
-    if n == 0:
-        x_ = np.sort(x)
-        w_ = w[x.argsort()]
-        return x_, w_
-    elif sum(w != 0) < n:
+    if sum(w != 0) < n:
         i = w.index
     else:
-        i = np.random.choice(w.index, size=n, replace=False, p=w/w.sum())
+        i = np.random.choice(w.index, size=n, replace=False)
 
     # Define sub-samples
     x_ = np.sort(x[i])
