18754757274ad75117aefbc4a11bfbfb4723699e
Will Handley <wh260@cam.ac.uk>
2021-05-27 18:31:55 +0100

    Added a statement about how many were equal

--- anesthetic/samples.py
+++ anesthetic/samples.py
@@ -741,14 +741,17 @@ class NestedSamples(MCMCSamples):
 
             invalid = samples.logL <= samples.logL_birth
             n_bad = invalid.sum()
+            n_equal = (samples.logL == samples.logL_birth).sum()
             if n_bad:
-                warnings.warn("%i out of %i samples have logL <= logL_birth."
+                warnings.warn("%i out of %i samples have logL <= logL_birth,"
+                              "\n%i of which have logL == logL_birth."
                               "\nThis may just indicate numerical rounding "
                               "errors at the peak of the likelihood, but "
                               "further investigation of the chains files is "
                               "recommended."
                               "\nDropping the invalid samples." %
-                              (n_bad, len(samples)), RuntimeWarning)
+                              (n_bad, len(samples), n_equal),
+                              RuntimeWarning)
                 samples = samples[~invalid].reset_index()
 
             samples['nlive'] = compute_nlive(samples.logL, samples.logL_birth)
