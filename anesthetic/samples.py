f6eca8fda479ed7c52ce4e0c5b71957238d4bcdc
Ormorod <Adam.Ormondroyd@gmail.com>
2022-06-16 22:02:00 +0100

    added validation to check for NaN values in logL

--- anesthetic/samples.py
+++ anesthetic/samples.py
@@ -94,6 +94,8 @@ class MCMCSamples(WeightedDataFrame):
             logL = kwargs.pop('logL', None)
             if logL is not None:
                 logL = np.where(logL <= logzero, -np.inf, logL)
+            if np.any(np.isnan(logL)):
+                raise ValueError("NaN found in likelihood values")
             self.tex = kwargs.pop('tex', {})
             self.limits = kwargs.pop('limits', {})
             self.label = kwargs.pop('label', None)
