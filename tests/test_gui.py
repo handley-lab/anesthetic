7e38dee354d54b694e60f1599adf0fe190e28c55
Will Handley <wh260@cam.ac.uk>
2022-07-31 08:56:07 +0100

    Updated for most recent PEP compliant assert statements

--- tests/test_gui.py
+++ tests/test_gui.py
@@ -9,31 +9,31 @@ def test_gui():
 
     # Type buttons
     plotter.type.buttons.set_active(1)
-    assert(plotter.type() == 'posterior')
+    assert plotter.type() == 'posterior'
     plotter.type.buttons.set_active(0)
-    assert(plotter.type() == 'live')
+    assert plotter.type() == 'live'
 
     # Parameter choice buttons
     plotter.param_choice.buttons.set_active(1)
-    assert(len(plotter.triangle.ax) == 2)
+    assert len(plotter.triangle.ax) == 2
     plotter.param_choice.buttons.set_active(0)
-    assert(len(plotter.triangle.ax) == 1)
+    assert len(plotter.triangle.ax) == 1
     plotter.param_choice.buttons.set_active(0)
     plotter.param_choice.buttons.set_active(2)
     plotter.param_choice.buttons.set_active(3)
-    assert(len(plotter.triangle.ax) == 4)
+    assert len(plotter.triangle.ax) == 4
 
     # Sliders
     plotter.evolution.slider.set_val(100)
-    assert(plotter.evolution() == 100)
+    assert plotter.evolution() == 100
     plotter.type.buttons.set_active(1)
 
     plotter.temperature.slider.set_val(0)
-    assert(plotter.temperature() == 1)
+    assert plotter.temperature() == 1
     plotter.temperature.slider.set_val(1)
-    assert(plotter.temperature() == 10)
+    assert plotter.temperature() == 10
     plotter.temperature.slider.set_val(2)
-    assert(plotter.temperature() == 100)
+    assert plotter.temperature() == 100
     plotter.type.buttons.set_active(0)
 
     if version.parse(mpl_version) >= version.parse('3.4.0'):
