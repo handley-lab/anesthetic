e884f46377a3f62b6ed208e0f79303d2e971f541
Ormorod <Adam.Ormondroyd@gmail.com>
2022-10-03 11:10:11 +0100

    copy _make_plot() from pandas #48888

--- anesthetic/plotting/_matplotlib/hist.py
+++ anesthetic/plotting/_matplotlib/hist.py
@@ -48,49 +48,62 @@ class HistPlot(_WeightedMPLPlot, _HistPlot):
         return bins
     
     def _make_plot(self) -> None:
-        from pandas.core.dtypes.common import is_extension_array_dtype
         from pandas.core.dtypes.missing import notna
-        from pandas.plotting._matplotlib.groupby import (
-            create_iter_data_given_by,
-        )
-        def remove_na_arraylike_idx(arr):
-            if is_extension_array_dtype(arr):
-                return notna(arr)
-            return notna(np.asarray(arr))
-        def reformat_hist_weights_given_y(y, weights, by):
-            if y.shape != weights.shape:
-                raise ValueError("data and weights must have the same shape")
-            if by is not None and len(y.shape) > 1:
-                return (
-                    np.array([wcol.T[remove_na_arraylike_idx(ycol)] for ycol, wcol in zip(y.T, weights.T)]).T,
-                )
-            return weights[remove_na_arraylike_idx(y)]
+        from pandas.io.formats.printing import pprint_thing
+        from pandas.plotting._matplotlib.groupby import create_iter_data_given_by, reformat_hist_y_given_by
+        colors = self._get_colors()
+        stacking_id = self._get_stacking_id()
 
-        kwds = self.kwds.copy()
-        weights = kwds.get("weights", None)
+        # Re-create iterated data if `by` is assigned by users
         data = (
             create_iter_data_given_by(self.data, self._kind)
             if self.by is not None
             else self.data
         )
-        
-        if weights is not None:
-            print(data.shape)
-            print(weights.shape)
-            if np.ndim(weights) != 1:
-                if data.shape != weights.T.shape:
-                    raise ValueError("weights must be broadcastable to same shape as data.")
-            else:
-                weights = np.broadcast_to(weights, data.T.shape)
-            # for i, (label, y) in enumerate(self._iter_data(data=data)):
-            #     temp_weights = weights
-            mask =  ~remove_na_arraylike_idx(data).T
-            print(mask.shape)
-            print(weights.shape)
-            weights = np.ma.masked_array(weights, mask=mask)
-            self.kwds["weights"] = np.squeeze(weights)
-            print(weights)
-        return super()._make_plot()
+
+        for i, (label, y) in enumerate(self._iter_data(data=data)):
+            ax = self._get_ax(i)
+
+            kwds = self.kwds.copy()
+
+            label = pprint_thing(label)
+            label = self._mark_right_label(label, index=i)
+            kwds["label"] = label
+
+            style, kwds = self._apply_style_colors(colors, kwds, i, label)
+            if style is not None:
+                kwds["style"] = style
+
+            kwds = self._make_plot_keywords(kwds, y)
+
+            # the bins is multi-dimension array now and each plot need only 1-d and
+            # when by is applied, label should be columns that are grouped
+            if self.by is not None:
+                kwds["bins"] = kwds["bins"][i]
+                kwds["label"] = self.columns
+                kwds.pop("color")
+
+            # We allow weights to be a multi-dimensional array, e.g. a (10, 2) array,
+            # and each sub-array (10,) will be called in each iteration. If users only
+            # provide 1D array, we assume the same weights is used for all iterations
+            weights = kwds.get("weights", None)
+            if weights is not None and np.ndim(weights) != 1:
+                weights = weights[:, i]
+
+            if weights is not None:
+                weights = weights[~isna(y)]
+                kwds["weights"] = weights
+
+            y = reformat_hist_y_given_by(y, self.by)
+
+            artists = self._plot(ax, y, column_num=i, stacking_id=stacking_id, **kwds)
+
+            # when by is applied, show title for subplots to know which group it is
+            if self.by is not None:
+                ax.set_title(pprint_thing(label))
+
+            self._append_legend_handles_labels(artists[0], label)
+
 
 
 class KdePlot(HistPlot, _KdePlot):
