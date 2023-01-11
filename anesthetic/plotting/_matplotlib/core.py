2b9e498ff6bd26ca0f8a0558f67ff52a05fb5550
Ormorod <Adam.Ormondroyd@gmail.com>
2023-01-11 14:47:21 +0000

    revert to before I started playing with AreaPlot

--- anesthetic/plotting/_matplotlib/core.py
+++ anesthetic/plotting/_matplotlib/core.py
@@ -141,14 +141,4 @@ class BarhPlot(_BarhPlot, _WeightedMPLPlot):
 
 class AreaPlot(_AreaPlot, _WeightedMPLPlot):
     # noqa: disable=D101
-    def _post_plot_logic(self, ax, data):
-        LinePlot._post_plot_logic(self, ax, data)
-
-        # is_shared_y = len(list(ax.get_shared_y_axes())) > 0
-        is_shared_y = False
-        # do not override the default axis behaviour in case of shared y axes
-        if self.ylim is None and not is_shared_y:
-            if (data >= 0).all().all():
-                ax.set_ylim(0, None)
-            elif (data <= 0).all().all():
-                ax.set_ylim(None, 0)
+    pass
