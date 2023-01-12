a364454f497ea50d99e23ae19206883aa665f0e9
Ormorod <Adam.Ormondroyd@gmail.com>
2023-01-12 13:32:53 +0000

    Revert "add plt.close("all") to test_HexBinPlot (which comes before test_AreaPlot)"
    This reverts commit 385f7f34d9078eedfe2aab9263672612028d3517.

--- tests/test_weighted_pandas.py
+++ tests/test_weighted_pandas.py
@@ -807,8 +807,6 @@ def test_HexBinPlot(mcmc_df, mcmc_wdf):
     wdf_colors = wdf_axes.collections[0].get_facecolors()
     assert_allclose(df_colors, wdf_colors)
 
-    plt.close("all")
-
 
 def test_AreaPlot(mcmc_df, mcmc_wdf):
     axes_wdf = mcmc_wdf.plot.area()
