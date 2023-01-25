ee1513debeb0829e4e09c79c08eeb6b67da01048
Will Handley <wh260@cam.ac.uk>
2023-01-25 10:15:41 +0000

    d_G check for perfect gaussian case

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -67,6 +67,8 @@ def test_perfect_ns_gaussian():
     D_KL = log_volume_n_ball(ndims) - ndims/2*np.log(2*np.pi*np.e*sigma**2)
     assert_allclose(samples.D_KL(), D_KL, atol=3*samples.D_KL(12).std())
 
+    assert_allclose(samples.d_G(), ndims, atol=3*samples.d_G(12).std())
+
     logZ = ndims/2 * np.log(2*np.pi*sigma**2) - log_volume_n_ball(ndims, R)
     assert_allclose(samples.logZ(), logZ, atol=3*samples.logZ(12).std())
 
