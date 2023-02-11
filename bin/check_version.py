0e31bbce87b67d44b157c88a8a21f98f4bb5d3b5
Will Handley <wh260@cam.ac.uk>
2023-02-11 17:49:57 +0000

    Now checking for version number matching

--- bin/check_version.py
+++ bin/check_version.py
@@ -3,18 +3,28 @@ import sys
 from packaging import version
 import subprocess
 vfile = "anesthetic/_version.py"
+README = "README.rst"
 
-current_version = subprocess.run(["cat", vfile],
-                                 stdout=subprocess.PIPE, text=True)
-previous_version = subprocess.run(["git", "show", "master:" + vfile],
-                                  stdout=subprocess.PIPE, text=True)
 
-current_version = current_version.stdout.split("=")[-1].strip().strip("'")
+def run(*args):
+    return subprocess.run(args, stdout=subprocess.PIPE, text=True).stdout
+
+current_version = run("cat", vfile)
+current_version = current_version.split("=")[-1].strip().strip("'")
 current_version = version.parse(current_version)
-previous_version = previous_version.stdout.split("=")[-1].strip().strip("'")
-previous_version = version.parse(previous_version)
 
-if current_version > previous_version:
-    sys.exit(0)
-else:
+#previous_version = run("git", "show", "master:" + vfile)
+#previous_version = previous_version.split("=")[-1].strip().strip("'")
+#previous_version = version.parse(previous_version)
+
+readme_version = run("grep", ":Version:", README)
+readme_version = readme_version.split(":")[-1].strip()
+readme_version = version.parse(readme_version)
+
+if current_version != readme_version:
+    sys.stderr.write("Version mismatch: {} != {}".format(vfile, README))
     sys.exit(1)
+
+#elif current_version <= previous_version:
+#    sys.stderr.write("Version must be incremented: {} <= {}".format(vfile, "master:" + vfile))
+#    sys.exit(1)
