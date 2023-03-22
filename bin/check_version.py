1779dad5b78342983095702f980050579d4cac82
Will Handley <wh260@cam.ac.uk>
2023-03-22 11:27:07 +0000

    Correct script in pyproject.toml + Improved packaging mark 7 (#271)
    * Added test for version increment
    
    * Moved to version checker workflow
    
    * Updated version checker
    
    * Updated naming of version checker
    
    * Job id must contain only alphanumeric characters
    
    * Bump version number
    
    * Corrected anesthetic gui command line script. Fixes #273
    
    * incremented version
    
    * Moved version checking into separate, tested script
    
    ---------
    
    Co-authored-by: Adam Ormondroyd <52655393+Ormorod@users.noreply.github.com>

--- bin/check_version.py
+++ bin/check_version.py
@@ -1,34 +1,35 @@
 #!/usr/bin/env python
 import sys
-from packaging import version
 import subprocess
+from packaging import version
+from utils import unit_incremented
 vfile = "anesthetic/_version.py"
 README = "README.rst"
 
 
 def run(*args):
-    return subprocess.run(args, stdout=subprocess.PIPE, text=True).stdout
+    return subprocess.run(args, text=True,
+                          stdout=subprocess.PIPE,
+                          stderr=subprocess.PIPE).stdout
 
 
 current_version = run("cat", vfile)
 current_version = current_version.split("=")[-1].strip().strip("'")
-current_version = version.parse(current_version)
 
 run("git", "fetch", "origin", "master")
 previous_version = run("git", "show", "remotes/origin/master:" + vfile)
 previous_version = previous_version.split("=")[-1].strip().strip("'")
-previous_version = version.parse(previous_version)
 
 readme_version = run("grep", ":Version:", README)
 readme_version = readme_version.split(":")[-1].strip()
-readme_version = version.parse(readme_version)
 
-HEAD = run("git", "rev-parse", "HEAD")
-master = run("git", "rev-parse", "remotes/origin/master")
-
-if current_version != readme_version:
+if version.parse(current_version) != version.parse(readme_version):
     sys.stderr.write("Version mismatch: {} != {}".format(vfile, README))
     sys.exit(1)
-elif HEAD != master and current_version <= previous_version:
-    sys.stderr.write("Version must be incremented: {} ({}) <= {} ({})".format(vfile, current_version, "master:" + vfile, previous_version))
+
+elif not unit_incremented(current_version, previous_version):
+    sys.stderr.write(("Version must be incremented by one:\n"
+                      "HEAD:   {},\n"
+                      "master: {}.\n"
+                      ).format(current_version, previous_version))
     sys.exit(1)
