4ea5195e006f6f837636f3b3d6815808f5df9110
AdamOrmondroyd <Adam.Ormondroyd@gmail.com>
2023-06-29 13:17:21 +0100

    only change axis labels to name if name exists

--- anesthetic/plotting/_matplotlib/core.py
+++ anesthetic/plotting/_matplotlib/core.py
@@ -93,8 +93,10 @@ class _PlanePlot2d(PlanePlot):
         )
         x = data[self.x]
         y = data[self.y]
-        self.x = x.name  # transfer the tex label
-        self.y = y.name  # transfer the tex label
+        if x.name:  # transfer the tex labels
+            self.x = x.name
+        if y.name:
+            self.y = y.name
         ax = self._get_ax(0)  # another one of these hard-coded 0s
 
         kwds = self.kwds.copy()
