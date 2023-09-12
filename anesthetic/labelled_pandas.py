ba7bf8e4ab6a9572eef0e9e56735f0232294e015
AdamOrmondroyd <Adam.Ormondroyd@gmail.com>
2023-09-12 15:57:28 +0200

    cast filled in labels to same type as given labels

--- anesthetic/labelled_pandas.py
+++ anesthetic/labelled_pandas.py
@@ -117,7 +117,8 @@ class _LabelledObject(object):
                 labels_map = index.to_frame().droplevel(labs)[labs]
                 if fill:
                     replacement = labels_map.loc[labels_map == ''].index
-                    labels_map.loc[labels_map == ''] = replacement
+                    labels_map.loc[labels_map == ''] = replacement.astype(
+                        labels_map.loc[labels_map != ''].dtype)
                 return labels_map
             else:
                 return index.to_series()
