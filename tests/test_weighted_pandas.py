remember loc to fix covariance coverage (cov cov lol)

tests/test_weighted_pandas.py
@@ -492,7 +492,7 @@ def test_WeightedSeries_cov(frame):
     assert_allclose(frame.A.cov(frame.A), 1./12, atol=1e-2)
     assert_allclose(frame.A.cov(frame.B), 0, atol=1e-2)
 
-    frame[0, 'A'] = np.nan
+    frame.loc[0, 'A'] = np.nan
     assert_allclose(frame.A.cov(frame.A), 1./12, atol=1e-2)
     assert_allclose(frame.A.cov(frame.B), 0, atol=1e-2)
 