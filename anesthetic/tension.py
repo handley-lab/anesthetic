d1f1c2a859bd96b06c8f260fb5f3ce871e1700c4
lukashergt <lthergt@phas.ubc.ca>
2024-04-08 16:32:26 -0700

    fix links and some formatting issues in `tension.py` docstrings

--- anesthetic/tension.py
+++ anesthetic/tension.py
@@ -3,62 +3,62 @@ from anesthetic.samples import Samples
 from scipy.stats import chi2
 
 
-def tension_stats(A, B, AB, nsamples=None, beta=None):
-    r"""Compute tension statistics between two samples.
+def tension_stats(A, B, AB, nsamples=None, beta=None):  # noqa: D301
+    """Compute tension statistics between two samples.
 
     Using nested sampling we can compute:
 
     - ``logZ``: Bayesian evidence
 
-        .. math::
-            \log Z = \log \int L \pi d\theta
+      .. math::
+          \\log Z = \\log \\int L \\pi d\\theta
 
     - ``D_KL``: Kullback--Leibler divergence
 
-        .. math::
-            D_\mathrm{KL} = \int P \log(P / \pi) d\theta
+      .. math::
+          D_\\mathrm{KL} = \\int P \\log(P / \\pi) d\\theta
 
     - ``logL_P``: posterior averaged log-likelihood
 
-        .. math::
-            \langle\log L\rangle_P = \int P \log L d\theta
+      .. math::
+          \\langle\\log L\\rangle_P = \\int P \\log L d\\theta
 
     - ``d_G``: Gaussian model dimensionality
-        (or posterior variance of the log-likelihood)
+      (or posterior variance of the log-likelihood)
 
-        .. math::
-            d_\mathrm{G}/2 = \mathrm{var}(\log L)_P
+      .. math::
+          d_\\mathrm{G}/2 = \\mathrm{var}(\\log L)_P
 
     - ``p``: p-value for the tension between two samples
 
-        .. math::
-            p = \int_{d_\mathrm{G} - 2 \log S}^{\infty} \chi^2 (x)dx
+      .. math::
+          p = \\int_{d_\\mathrm{G} - 2 \\log S}^{\\infty} \\chi^2 (x)dx
 
     Parameters
     ----------
-    A : :class:`Samples`/:class:`NestedSamples`
-        Dataset A
-        List or array-like of one or more nested sampling runs
+    A : :class:`anesthetic.samples.Samples` or \
+        :class:`anesthetic.samples.NestedSamples`
+        (Nested) Samples from a sampling run using only dataset A.
 
-    B : :class:`Samples`/:class:`NestedSamples`
-        Dataset B
-        List or array-like of one or more nested sampling runs
+    B : :class:`anesthetic.samples.Samples` or \
+        :class:`anesthetic.samples.NestedSamples`
+        (Nested) Samples from a sampling run using only dataset B.
 
-    AB : :class:`Samples`/:class:`NestedSamples`
-        Dataset AB
-        List or array-like of one or more nested sampling runs
+    AB : :class:`anesthetic.samples.Samples` or \
+         :class:`anesthetic.samples.NestedSamples`
+        (Nested) Samples from a sampling run using datasets A and B jointly.
 
     nsamples : int, optional
         - If nsamples is not supplied, calculate mean value
         - If nsamples is integer, draw nsamples from the distribution of
-            values inferred by nested sampling
+          values inferred by nested sampling
 
     beta : float, array-like, optional
         inverse temperature(s) beta=1/kT. Default 1
 
     Returns
     -------
-    samples_stats : :class:`pandas.DataFrame`
+    samples_stats : :class:`anesthetic.samples.Samples`
         DataFrame containing the following tension statistics:
         logZ, D_KL, logL_P, d_G, p
     """
