7e90ac5bbe63c7220852c4d5b08a0c8dad7e44ce
Will Handley <wh260@cam.ac.uk>
2024-09-19 10:19:04 +0100

    replaced \\m

--- anesthetic/tension.py
+++ anesthetic/tension.py
@@ -11,34 +11,34 @@ def stats(A, B, AB, nsamples=None, beta=None):  # noqa: D301
     - ``logR``: Logarithmic of R statistic
 
       .. math::
-        \\log \mathcal{R} = \\log \mathcal{Z}_\\mathrm{AB} 
-                            - \\log \mathcal{Z}_\\mathrm{A}
-                            - \\log \mathcal{Z}_\\mathrm{B}
+        \\log\\mathcal{R} = \\log\\mathcal{Z}_\\\mathrm{AB} 
+                            - \\log\\mathcal{Z}_\\mathrm{A}
+                            - \\log\\mathcal{Z}_\\mathrm{B}
 
     - ``logI``: Logarithmic of information ratio
 
       .. math::
-        \\log \mathcal{I} = \mathcal{D}_\\mathrm{KL}^A 
-                            + \mathcal{D}_\\mathrm{KL}^B 
-                            - \mathcal{D}_\\mathrm{KL}^{AB}
+        \\log\mathcal{I} = \\mathcal{D}_\\mathrm{KL}^A 
+                            + \\mathcal{D}_\\mathrm{KL}^B 
+                            - \\mathcal{D}_\\mathrm{KL}^{AB}
 
     - ``logS``: Logarithmic of suspiciousness
 
       .. math::
-        \\log \mathcal{S} = \\log \mathcal{L}_\\mathrm{AB} 
-                            - \\log \mathcal{L}_\\mathrm{A} 
-                            - \\log \mathcal{L}_\\mathrm{B}
+        \\log\mathcal{S} = \\log\\mathcal{L}_\\mathrm{AB} 
+                            - \\log\\mathcal{L}_\\mathrm{A} 
+                            - \\log\\mathcal{L}_\\mathrm{B}
 
     - ``d_G``: Gaussian model dimensionality
       (or posterior variance of the log-likelihood)
 
       .. math::
-        d_\\mathrm{G}/2 = \\mathrm{var}(\\log \mathcal{L})_\mathcal{P}
+        d_\\mathrm{G}/2 = \\mathrm{var}(\\log\mathcal{L})_\mathcal{P}
 
     - ``p``: p-value for the tension between two samples
 
       .. math::
-        p = \\int_{d_\\mathrm{G} - 2 \\log \mathcal{S}}^{\\infty} \\chi^2 (x)dx
+        p = \\int_{d_\\mathrm{G} - 2 \\log\mathcal{S}}^{\\infty} \\chi^2 (x)dx
 
     Parameters
     ----------
