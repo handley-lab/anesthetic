c131d69c02fc827036bc49d2de2f5af4efb0d008
dilyong <dlo26@cam.ac.uk>
2025-02-20 15:57:04 +0000

    Updated the read_csv() function such that it can not only read filename, but can also read BytesI0 objects, which is the file type for ns and mcmc samples downloaded from Zenodo.

--- anesthetic/read/csv.py
+++ anesthetic/read/csv.py
@@ -6,9 +6,15 @@ from pathlib import Path
 
 def read_csv(filename, *args, **kwargs):
     """Read a CSV file into a :class:`Samples` object."""
-    filename = Path(filename)
-    kwargs['label'] = kwargs.get('label', filename.stem)
-    wldf = wl_read_csv(filename.with_suffix('.csv'))
+    #filename = Path(filename)
+    try: 
+        filename = Path(filename)
+        kwargs['label'] = kwargs.get('label', filename.stem)
+        filename = filename.with_suffix('.csv')
+    except TypeError:
+        pass
+    #kwargs['label'] = kwargs.get('label', filename.stem)
+    wldf = wl_read_csv(filename)
     if 'nlive' in wldf.columns:
         return NestedSamples(wldf, *args, **kwargs)
     else:
