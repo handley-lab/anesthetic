b5d18a4f4c89231888dc2807d0b6683163ef3690
lukashergt <lthergt@phas.ubc.ca>
2026-01-12 11:37:01 +0100

    use the same error tolerances for axis limit tests, independent of plotting types, now that we have the more exact limit estimates using mpl margins

--- tests/test_plot.py
+++ tests/test_plot.py
@@ -702,8 +702,6 @@ def test_q_2d(plot_2d):
     d = np.random.randn(2, 1000)
 
     def get_data_from_plot(plot_2d):
-        rel1 = 0.1
-        rel2 = 1e-15
         if plot_2d == kde_contour_plot_2d:
             x, y = p[0].allsegs[0][0].T
         elif plot_2d == hist_plot_2d:
@@ -711,42 +709,41 @@ def test_q_2d(plot_2d):
             y = p.get_coordinates()[:, 0, 1]
         elif plot_2d == scatter_plot_2d:
             x, y = p[0].get_xydata().T
-            rel1 = 0.2  # bigger than for contour or hist
-        return x, y, rel1, rel2
+        return x, y
 
     def expected_limits(plot_2d, x, y):
         if plot_2d == scatter_plot_2d:
-            xpad = (x.max()-x.min()) * matplotlib.rcParams['axes.xmargin']
-            ypad = (y.max()-y.min()) * matplotlib.rcParams['axes.ymargin']
+            xpad = (x.max()-x.min()) * plt.rcParams['axes.xmargin']
+            ypad = (y.max()-y.min()) * plt.rcParams['axes.ymargin']
             return (x.min()-xpad, x.max()+xpad), (y.min()-ypad, y.max()+ypad)
         return (x.min(), x.max()), (y.min(), y.max())
 
     fig, ax = plt.subplots()
     # check that axis limits match data limits for single plot
     p = plot_2d(ax, d[0], d[1], q=1)  # int input for q
-    x, y, rel1, rel2 = get_data_from_plot(plot_2d)
+    x, y = get_data_from_plot(plot_2d)
     xlim, ylim = expected_limits(plot_2d, x, y)
-    assert ax.get_xlim() == pytest.approx((-1, 1), rel=rel1)
-    assert ax.get_ylim() == pytest.approx((-1, 1), rel=rel1)
-    assert xlim == pytest.approx(ax.get_xlim(), rel=rel2)
-    assert ylim == pytest.approx(ax.get_ylim(), rel=rel2)
+    assert ax.get_xlim() == pytest.approx((-1, 1), rel=0.2)
+    assert ax.get_ylim() == pytest.approx((-1, 1), rel=0.2)
+    assert ax.get_xlim() == pytest.approx(xlim, rel=1e-15)
+    assert ax.get_ylim() == pytest.approx(ylim, rel=1e-15)
 
     # check that axis limits grow with new, larger plot
     p = plot_2d(ax, d[0], d[1], q="2sigma")  # str input for q
-    x, y, rel1, rel2 = get_data_from_plot(plot_2d)
+    x, y = get_data_from_plot(plot_2d)
     xlim, ylim = expected_limits(plot_2d, x, y)
-    assert ax.get_xlim() == pytest.approx((-2, 2), rel=rel1)
-    assert ax.get_ylim() == pytest.approx((-2, 2), rel=rel1)
-    assert xlim == pytest.approx(ax.get_xlim(), rel=rel2)
-    assert ylim == pytest.approx(ax.get_ylim(), rel=rel2)
+    assert ax.get_xlim() == pytest.approx((-2, 2), rel=0.2)
+    assert ax.get_ylim() == pytest.approx((-2, 2), rel=0.2)
+    assert ax.get_xlim() == pytest.approx(xlim, rel=1e-15)
+    assert ax.get_ylim() == pytest.approx(ylim, rel=1e-15)
 
     # check that axis limits do not shrink with new, smaller plot
     p = plot_2d(ax, d[0], d[1], q=(0.025, 0.84))  # tuple input for q
-    x, y, rel1, rel2 = get_data_from_plot(plot_2d)
-    assert ax.get_xlim() == pytest.approx((-2, 2), rel=rel1)
-    assert ax.get_ylim() == pytest.approx((-2, 2), rel=rel1)
-    assert (x.min(), x.max()) == pytest.approx((-2, 1), rel=rel1)
-    assert (y.min(), y.max()) == pytest.approx((-2, 1), rel=rel1)
+    x, y = get_data_from_plot(plot_2d)
+    assert ax.get_xlim() == pytest.approx((-2, 2), rel=0.2)
+    assert ax.get_ylim() == pytest.approx((-2, 2), rel=0.2)
+    assert (x.min(), x.max()) == pytest.approx((-2, 1), rel=0.2)
+    assert (y.min(), y.max()) == pytest.approx((-2, 1), rel=0.2)
 
 
 def test_kde_plot_nplot():
