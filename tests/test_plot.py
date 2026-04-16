7036c3093a267dbf51cd9975bf25c5a32077156c
lukashergt <lthergt@phas.ubc.ca>
2026-04-16 03:02:05 +0200

    update tests for the new `tricontour` usage instead of `contour`

--- tests/test_plot.py
+++ tests/test_plot.py
@@ -963,7 +963,7 @@ def test_logscale_2d(plot_2d):
     p = plot_2d(ax, x, logy)
     if 'kde' in plot_2d.__name__:
         if version.parse(matplotlib.__version__) >= version.parse('3.8.0'):
-            xmax, ymax = p[0].get_paths()[1].vertices[0].T
+            xmax, ymax = p[0].get_paths()[1].vertices.T
         else:
             xmax, ymax = p[0].allsegs[1][0].T
         xmax = np.mean(np.log10(xmax))
@@ -987,7 +987,7 @@ def test_logscale_2d(plot_2d):
     p = plot_2d(ax, logx, y)
     if 'kde' in plot_2d.__name__:
         if version.parse(matplotlib.__version__) >= version.parse('3.8.0'):
-            xmax, ymax = p[0].get_paths()[1].vertices[0].T
+            xmax, ymax = p[0].get_paths()[1].vertices.T
         else:
             xmax, ymax = p[0].allsegs[1][0].T
         xmax = np.mean(xmax)
@@ -1012,7 +1012,7 @@ def test_logscale_2d(plot_2d):
     p = plot_2d(ax, x, y)
     if 'kde' in plot_2d.__name__:
         if version.parse(matplotlib.__version__) >= version.parse('3.8.0'):
-            xmax, ymax = p[0].get_paths()[1].vertices[0].T
+            xmax, ymax = p[0].get_paths()[1].vertices.T
         else:
             xmax, ymax = p[0].allsegs[1][0].T
         xmax = np.mean(np.log10(xmax))
